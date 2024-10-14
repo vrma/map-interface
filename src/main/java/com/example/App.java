bc481db5137cbfca663790fde2a8f458718ba33b
vrma <vrmachado@gmail.com>
2024-10-14 19:43:34 +0200

    flatMap explicado

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
  * 
  */
 public class App {
+    
     public static void main(String[] args) {
 
         /*
---
+++ src/main/java/com/example/FlatMapDemo.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FlatMapDemo {
+    
+    public static void main(String[] args) {
+        
+        // El metodo flatMap aplana listas, es muy util cuando se tienen
+        // listas anidadas, y ademas no tendria en cuenta alguna lista
+        // que estuviese vacia.
+
+        List<List<String>> listaDeLista = Arrays.asList(
+            Arrays.asList("Ruben", "Antonio"),
+            Arrays.asList("Juan"),
+            Arrays.asList("Marco")
+
+        );
+
+        listaDeLista.stream().flatMap(l -> l.stream()).forEach(System.out::println);
+
+        List<String> lista1 = List.of("Ivan", "Juan");
+        List<String> lista2 = List.of("Antonio");
+        List<String> lista3 = List.of("Ruben", "Marcos");
+
+        Stream<List<String>> flujoDeListas = Stream.of(lista1, lista2, lista3);
+
+        System.out.println("Flujo de listas aplanado por cortesia de flatMap");
+        flujoDeListas.flatMap(l -> l.stream()).forEach(System.out::println);
+
+    }
+
+}
